4033ec6a20d1438d9bd409f7056237686594fe04
Thomas Ricci <git@thomasricci.dev>
2023-05-22 13:43:36 -0400

    disable pages for now

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import NavbarLayout from "@theme/Navbar/Layout";
 import NavbarContent from "@theme/Navbar/Content";
-import routes from "@generated/routes";
 
 // WARNING: this is an unsafe swizzled component (including all subdirs of Navbar). might break on the next major version
 
@@ -9,11 +8,20 @@ export default function Navbar({ disabled }) {
   if(window.location.pathname.includes("/.")) {
     return <Fragment/>;
   }
+
   // Disable breadcrumbs in navbar if the current page doesn't support them
-  let breadcrumbsDisabled = false;
-  if(routes.find(value => value.path.replace("/", "") === window.location.pathname.replace("/", "")) !== undefined) {
-    breadcrumbsDisabled = true;
-  }
+  const breadcrumbsDisabled = false;
+
+
+  // DONT REMOVE! might need later idk
+  // if we use the src/pages, we'll need this. however it is currently unstable.
+  //
+  // please do not use the src/pages directory
+
+  // if(routes.find(value => value.path.replace("/", "") === window.location.pathname.replace("/", "") && !("routes" in value)) !== undefined) {
+  //   breadcrumbsDisabled = true;
+  // }
+
   return (
     <Fragment>
       <NavbarLayout disabled={disabled} >
