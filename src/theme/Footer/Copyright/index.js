77277d44c1ce5fdc51cd0311e77ed3413e279715
Thomas Ricci <git@thomasricci.dev>
2023-05-28 17:25:46 -0400

    update footer

--- src/theme/Footer/Copyright/index.js
+++ src/theme/Footer/Copyright/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+
 export default function FooterCopyright({ copyright }) {
   return (
     <Fragment>
@@ -12,7 +13,7 @@ export default function FooterCopyright({ copyright }) {
         />
         <div className={"footer__copyright__spacer"}/>
         <div className={"footer__copyright__attr"} style={{ whiteSpace: "nowrap" }}>
-          Built by <wbr/><a href={"https://www.thomasricci.dev"}>Thomas Ricci</a> <wbr/>and <a href={"https://api.github.com/repos/XaverianTeamRobotics/FtcRobotController/contributors"}>these great people</a>.
+          Built by <wbr/><a href={"https://www.thomasricci.dev"}>Thomas Ricci</a> <wbr/>and <a href={"https://github.com/XaverianTeamRobotics/FtcRobotController/graphs/contributors"}>these great people</a>.
         </div>
       </div>
     </Fragment>
